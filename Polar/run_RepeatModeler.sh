9ca186723a69485c665c8db46be00e6ca077ad83
sb65 <sb65@polar.life.illinois.edu>
2024-01-25 14:41:19 -0600

    Polar repeatmasker

--- Polar/run_RepeatModeler.sh
+++ Polar/run_RepeatModeler.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-#combine the database of Repeatmodeler and Zebrafish from repbase
 
-repeat_modeler_path=
-repbase_path=
+#conda activate RepeatMasking
+#paths to RepeatModeler and genome assembly
+src=/home/sb65/tools/RepeatModeler/RepeatModeler-2.0.3
+fasta=/data/work/Dmaw12/Dmaw12_-_Scaffold_-_arks_cur_results_2856604_bionano/cur_results_2856605/hybrid_scaffolds/EXP_REFINEFINAL1_bppAdjust_cmap_Dmaw12_3cell_HifiasmV0_16_purge_haplotigs_arks_scaffolds_fa_NGScontigs_HYBRID_SCAFFOLD.fasta
 
-cat $repeat_modeler_path $repbase_path > curated_repeats.fa
+# RepModeler Build Database
+$src/BuildDatabase -name asm.db -engine ncbi $fasta
 
-#Run Repeat Masker
-bin=/home/sb65/tools/RepeatMasker/RepeatMasker
-dir=$PWD
-#Genome assembly fasta file
-genome=
-
-/data/home/sb65/tools/RepeatMasker/RepeatMasker/RepeatMasker -a -lib ${dir}/curated_repeats.fa $genome -poly -gff -pa 50 -xsmall -dir ${dir} | tee tmux.log
+# RepModeler Model Repeats
+$src/RepeatModeler -database asm.db -engine ncbi -pa 45 -LTRStruct
